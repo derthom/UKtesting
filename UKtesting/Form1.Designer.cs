0b7364fb753c6a428736855b966e1cee12faa918
Derrick Thomas <derrick.thomas18@yahoo.in>
2017-02-17 06:50:37 +0530

    UK Validation

--- UKtesting/Form1.Designer.cs
+++ UKtesting/Form1.Designer.cs
@@ -28,12 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(139, 49);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(139, 89);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(139, 143);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 154);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- UKtesting/Form1.cs
+++ UKtesting/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using UKtesting.ServiceReference1;
 
 namespace UKtesting
 {
@@ -15,6 +17,44 @@ namespace UKtesting
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var ukxml = XDocument.Load(@"C:\Users\derrthom\Desktop\data.xml");
+
+            var county = from c in ukxml.Root.Descendants("Table") select c.Element("County").Value;
+            var town = from c in ukxml.Root.Descendants("Table") select c.Element("Town").Value;
+            var pc = from c in ukxml.Root.Descendants("Table") select c.Element("PostCode").Value;
+            foreach (string key1 in county)
+            comboBox1.Items.Add(key1);
+            //var town = from c in ukxml.Root.Descendants("Table") where c.Element("County").Value = (comboBox1.SelectedText.ToUpper()) select c.Element("Town").Value;
+            foreach (string key2 in town )
+                //var town1= from c in ukxml.Root.Descendants("Table") where (string)c.Attribute("Town")=
+               //comboBox2.Items.Add(ukxml.Root.Descendants("Table").Select(x=>x.Element("Town")).Where(y=>y.Element("County")=))
+                comboBox2.Items.Add(key2);
+            foreach (string key3 in pc)
+                comboBox3.Items.Add(key3);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.UKLocationSoapClient kkk = new ServiceReference1.UKLocationSoapClient("UKLocationSoap12");
+            string c = comboBox1.SelectedItem.ToString();
+            Console.WriteLine(c);
+            string t = comboBox2.SelectedItem.ToString();
+            string p = comboBox3.SelectedItem.ToString();
+            string result = kkk.ValidateUKAddress(t,c,p);
+             if(result== "<NewDataSet />")
+             {
+                richTextBox1.Text = "NOT A VALID ADDRESS";
+             }
+             else
+             {
+               richTextBox1.Text = "SUCCESSFULLY VALIDATED";
+             }  
+            
+            
         }
     }
 }
